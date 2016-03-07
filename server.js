fd122be04dcdaf73e2fa47d0b7625628ab7d526a
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-07 12:18:43 -0600

    haven't decided to render it in html or jade

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Testing');
+  res.render('index');
 });
 
 app.listen(PORT, () => {
