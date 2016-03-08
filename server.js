393d8b534a1525f2f529d5c132cae80cfc2fdb4a
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-07 21:45:00 -0600

    it works, but not as well as I would like

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 'use strict';
 
-const express = require('express');
-const app = express();
 const API = require('./API') // Anyone cloning this will need there own API-Key
-const request = require('request');
 const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const request = require('request');
+const app = express();
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017; // eslint-disable-line no-magic-numbers
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
+const MONGODB_URL_PREFIX = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@`: '';
 
+const MONGO_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/civic-citizen`;
 
-app.use(express.static('public'));
+// app.set('view engine', 'html');
+
+// app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded( {extended: false} ) );
 app.use(bodyParser.json() );
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile('/index');
 });
 
-app.post('/', (req, res) => {
+app.get('/repfind', (req, res) => {
+  res.sendFile('/repfind.html');
+});
+
+app.post('/repfind', (req, res) => {
   console.log("What did I type in?", req.body);
 
   // let searchTerms = '2317+Haskell+Drive';
@@ -31,6 +45,11 @@ app.post('/', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Node.js server started. Listening on port ${PORT}`);
+
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Node.js server started. Connected to Database. Listening on port ${PORT}`);
+  });
 });
