9122124804e75e314c605f6ea14a4d15814499ef
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-17 09:09:00 -0500

    loop over the elections using jade

--- controllers/action.js
+++ controllers/action.js
@@ -8,5 +8,56 @@ module.exports.display = (req, res) => {
 };
 
 module.exports.getInfo = (req, res) => {
-  res.render('action');
+
+  let searchTerms = req.body.address;
+  const electionUrl = `https://www.googleapis.com/civicinfo/v2/elections?key=${API.civicKey}`;
+
+  request.get(electionUrl, (err, response, data) => {
+    if (err) throw err;
+
+    let elections = JSON.parse(data);
+    elections = elections.elections;
+    elections.shift();
+
+    const voterInfoUrl = `https://www.googleapis.com/civicinfo/v2/voterinfo?address=${searchTerms}&electionId=2000&key=${API.civicKey}`;
+
+    request.get(voterInfoUrl, (err, response, data) => {
+      if (err) throw err;
+
+      let parsedData = JSON.parse(data);
+      let voterinfo = parsedData.state
+
+      let stateElections = [];
+      for (let i = 0; i < parsedData.contests.length; i++) {
+        if (parsedData.contests[i].type === "General") {
+          stateElections.push(parsedData.contests[i]);
+        }
+      }
+
+
+      let localElectionInfoUrl;
+      if (voterinfo[0].local_jurisdiction) {
+        localElectionInfoUrl = voterinfo[0].local_jurisdiction.electionAdministrationBody.electionInfoUrl;
+      } else {
+        localElectionInfoUrl = "Not Found";
+      }
+
+      let everything = {
+        voterState: voterinfo[0].name,
+        stateElectionInfo: voterinfo[0].electionAdministrationBody.electionInfoUrl,
+        localElectionInfo: localElectionInfoUrl,
+        registrationStatus: voterinfo[0].electionAdministrationBody.electionRegistrationConfirmationUrl,
+        registrationToVote: voterinfo[0].electionAdministrationBody.electionRegistrationUrl,
+        whereToVote: voterinfo[0].electionAdministrationBody.votingLocationFinderUrl,
+        userAddress: parsedData.normalizedInput,
+        allElections: elections,
+        elections: parsedData.contests,
+        stateElections: stateElections,
+        else: parsedData
+      }
+      console.log("elections", elections);
+      res.render("action", {actionInfo: everything});
+    })
+  })
+
 };
