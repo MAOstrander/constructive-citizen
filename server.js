1b9d996dc564889bc0b082544ff3ab6ed3394d0c
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-18 15:23:19 -0500

    added some middleware and began the login/creation process

--- server.js
+++ server.js
@@ -3,10 +3,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const request = require('request');
+const session = require('express-session');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'iamabanana';
 const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
 const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
 const MONGODB_USER = process.env.MONGODB_USER || '';
@@ -22,6 +25,27 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded( {extended: false} ) );
 app.use(bodyParser.json() );
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use((req, res, next) => {
+  req.session.visits = req.session.visits || {};
+  req.session.visits[req.url] = req.session.visits[req.url] || 0;
+  req.session.visits[req.url]++;
+
+  console.log(req.session.visits);
+  next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 app.use(routes);
 
