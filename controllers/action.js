3597d13e222de6e862c801884cac56b542f12b5f
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-24 09:56:04 -0500

    voter information is returned and searched accordingly

--- controllers/action.js
+++ controllers/action.js
@@ -5,55 +5,7 @@ const API = require('../API') // Anyone cloning this will need their own API-Key
 const Person = require('../models/person');
 const Vote = require('../models/vote');
 
-module.exports.display = (req, res) => {
-
-  if (res.locals.user) {
-    Vote.findOne({ userID: res.locals.user._id }, function (err, voter) {
-      if (err) throw err;
-
-      if (voter) {
-        console.log("Displaying stored data");
-        const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
-        const diff = new Date() - voter._id.getTimestamp() - ONE_DAY_IN_MS;
-        const lessThanDayAgo = diff < 0;
-
-        if (lessThanDayAgo) {
-          res.render('action', {actionInfo: voter});
-        } else {
-          console.log("WHATNOW?");
-        }
-      }
-      // else {
-      //   Person.findOne({ _id: res.locals.user._id }, function (err, person) {
-      //     if (err) throw err;
-
-      //     let searchTerms = `${person.address} ${person.city} ${person.state} ${person.zip}`;
-      //     console.log("searchTerms", searchTerms);
-
-      //     var reps = new Promise( (resolve, reject) => {
-      //         apiSearch(searchTerms, req, res, resolve);
-      //       });
-
-      //     reps.then( val => {
-      //         console.log("results from apiSearch:", val);
-      //         res.render('find', {personsReps: val});
-      //       }
-      //     );
-
-      //   });
-      // }
-    });
-  } else {
-    res.render('action');
-  }
-
-};
-
-
-
-module.exports.getInfo = (req, res) => {
-
-  let searchTerms = req.body.address;
+function voteApiSearch(searchTerms, req, res, resolve) {
   const electionUrl = `https://www.googleapis.com/civicinfo/v2/elections?key=${API.civicKey}`;
 
   request.get(electionUrl, (err, response, data) => {
@@ -116,25 +68,82 @@ module.exports.getInfo = (req, res) => {
 
           if (voter) {
             console.log("You already have Info!");
-            // resolve(everything);
-            res.render("action", {actionInfo: everything});
+            resolve(everything);
           } else {
             everything.save( (err) => {
               if (err) throw err;
 
               console.log("SAVED ELECTIONS!");
-              // resolve(everything);
-              res.render("action", {actionInfo: everything});
+              resolve(everything);
             });
           }
         });
       } else {
         console.log("Did NOT save elections", elections);
-        // resolve(everything);;
-        res.render("action", {actionInfo: everything});
+        resolve(everything);;
       }
 
     })
   })
+}
+
+module.exports.display = (req, res) => {
+
+  if (res.locals.user) {
+    Person.findOne({ _id: res.locals.user._id }, function (err, person) {
+      if (err) throw err;
+
+      let searchTerms = `${person.address} ${person.city} ${person.state} ${person.zip}`;
+      console.log("searchTerms", searchTerms);
+
+      Vote.findOne({ userID: res.locals.user._id }, function (err, voter) {
+        if (err) throw err;
+
+        let lessThanDayAgo = false;
+        if (voter) {
+          console.log("Displaying stored data");
+          const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+          const diff = new Date() - voter._id.getTimestamp() - ONE_DAY_IN_MS;
+          lessThanDayAgo = diff < 0;
+        }
+
+        if (lessThanDayAgo && voter) {
+          console.log("recent times");
+            res.render('action', {actionInfo: voter});
+        } else {
+
+          var newVote = new Promise( (resolve, reject) => {
+            voteApiSearch(searchTerms, req, res, resolve);
+          });
+
+          newVote.then( val => {
+            console.log("results from voteApiSearch:", val);
+            res.render('action', {actionInfo: val});
+          });
+        }
+
+      }); // Vote.findone
+    }); // Person.findone
+
+  } else {
+    res.render('action');
+  }
+
+};
+
+
+
+module.exports.getInfo = (req, res) => {
+
+  let searchTerms = req.body.address;
+
+  var newVote = new Promise( (resolve, reject) => {
+    voteApiSearch(searchTerms, req, res, resolve);
+  });
+
+  newVote.then( val => {
+    console.log("results from voteApiSearch:", val);
+    res.render('action', {actionInfo: val});
+  });
 
 };
