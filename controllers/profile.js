bab914199cee6de5b3a4cdfd365cd80cce409f6f
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-03-27 18:23:47 -0500

    changing citizenship updates in the profile, plus removed unused images

--- controllers/profile.js
+++ controllers/profile.js
@@ -42,13 +42,15 @@ module.exports.changeAddress = (req, res) => {
   const dob = res.locals.user.dob
   const now = new Date();
   // If the person is not a citizen or less than 18 they can't currently vote
-  let canVote = req.body.citizen;
-  if ((now - dob) < (18 * 365 * 24 * 60 * 60 * 1000)) {
+  let isCitizen = req.body.citizen;
+  let canVote = isCitizen;
+  let overEighteen = (now - dob) > (18 * 365 * 24 * 60 * 60 * 1000);
+  if (!overEighteen) {
     canVote = false;
   }
 
   var conditions = { _id: res.locals.user._id };
-  var update = { $set: { address:req.body.street, city: req.body.city, state: req.body.state, zip: req.body.zip, canVote: canVote}};
+  var update = { $set: { address:req.body.street, city: req.body.city, state: req.body.state, zip: req.body.zip, isCitizen: isCitizen, overEighteen: overEighteen, canVote: canVote}};
   var options = { upsert: true };
 
   Person.update(conditions, update, options, (err, say) => {
